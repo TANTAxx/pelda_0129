a0bffd398b31c4f989296117a9ebfc04d8defa95
Tancsics Richard <tanta1324@gmail.com>
2022-01-29 16:14:05 +0100

    Uplift Log4J to 1.2.17

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class UtilTest {
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UtilTest {
+    @Test
+    public void shouldReturnMinusOneWhenEmptryArray() {
+        int result = Utils.maxker(new int[]{});
+        assertEquals(- 1, result);
+    }
+
+    @Test
+    public void shouldReturnMinusOneWhenNullArray() {
+        int result = Utils.maxker(null);
+        assertEquals(- 1, result);
+    }
+
+    @Test
+    public void shouldReturnCorrectIndexOnIntegerMINVALUE() {
+        int result = Utils.maxker(new int [] {Integer.MIN_VALUE});
+        assertEquals(0, result);
+    }
 }
